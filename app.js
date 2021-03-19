a4a509c18c2316846f363d24a70393ce6ab3eb16
Issam Merikhi <issammerikhi@outlook.fr>
2021-03-19 16:00:22 +0100

    container+css

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const txtAnim = document.querySelector('h3');
 txtAnim.style.textAlign = "center";
 txtAnim.style.fontFamily = "monospace";
@@ -12,3 +14,25 @@ new Typewriter(txtAnim, {
 .start()
 
 
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+
+
+const options = {
+  threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries){
+  entries.forEach(entry => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    
+  });
+
+}
+
+sections.forEach(section => {
+  observer.observe(section)
+})
