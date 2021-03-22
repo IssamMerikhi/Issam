24a88ef2931f6d41bcb406fda6e5e83a1b5fd8b0
Issam Merikhi <issammerikhi@outlook.fr>
2021-03-22 20:05:18 +0100

    change a lot of things

---
+++ app2.js
@@ -0,0 +1,39 @@
+
+
+const txtAnim = document.querySelector('h3');
+txtAnim.style.textAlign = "center";
+txtAnim.style.fontFamily = "monospace";
+txtAnim.style.fontSize = "300%";
+txtAnim.style.color = "white";
+
+
+
+
+new Typewriter(txtAnim, {
+  deleteSpeed: 20
+}).typeString('hi , Issam here.')
+.start()
+
+
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('.bubble');
+
+
+const options = {
+  threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries){
+  entries.forEach(entry => {
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    
+  });
+
+}
+
+sections.forEach(section => {
+  observer.observe(section)
+})
