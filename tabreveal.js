49791e46902e272524775145db9bf7eabffaf1df
Issam Merikhi <issammerikhi@outlook.fr>
2021-03-30 00:51:35 +0200

    add some effects

---
+++ tabreveal.js
@@ -0,0 +1,24 @@
+/*window.onload = function(){
+    const EFFECT = document.getElementById("t");
+
+    window.addEventListener('scroll2', scrollEffect2);
+
+    function scrollEffect2(){
+        if(window.scrollY >= 500){
+            EFFECT.style.top = '53rem';
+            EFFECT.style.left = '20%';
+            EFFECT.style.opacity = '1';
+            EFFECT.style.transform = 'translateX(0px)';
+            EFFECT.style.transition = '1s ease-in-out';
+        } 
+        else {
+            EFFECT.style.opacity = '0';
+            EFFECT.style.transform = 'translateX(-50px)';
+
+        }
+    }
+
+    scrollEffect2();
+}
+*/
+
