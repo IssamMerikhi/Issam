9b02ca99f5fc1717139e9feed885597dde9bd2c7
Issam Merikhi <issammerikhi@outlook.fr>
2021-04-01 20:04:29 +0200

    linkedin url

--- textanim.js
+++ textanim.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#typed', {
-    strings: ['welcome','Say Hello', 'Ask me something'],
+    strings: ['welcome !','say hello ?', 'ask me something...'],
     typeSpeed : 10,
     backSpeed : 50, 
     loop : true,
